cf64c88770404991a478369b6dd3e3c3875cf3a2
sherrywongls <sherrywong@berkeley.edu>
2013-11-25 11:44:41 -0800

    Removed check.xml for now because of compiling error

--- gen/edu/berkeley/cs160/qUp/R.java
+++ gen/edu/berkeley/cs160/qUp/R.java
@@ -21,36 +21,43 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_qup_tag_in_off=0x7f020000;
+        public static final int btn_qup_tag_in_on=0x7f020001;
+        public static final int check=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080008;
-        public static final int View1=0x7f080009;
-        public static final int action_settings=0x7f080016;
-        public static final int app_layout=0x7f08000a;
-        public static final int app_map=0x7f08000c;
-        public static final int app_reservation=0x7f08000b;
-        public static final int business_layout_0=0x7f08000d;
-        public static final int business_layout_1=0x7f080010;
-        public static final int business_layout_2=0x7f080013;
-        public static final int business_name_0=0x7f08000e;
-        public static final int business_name_1=0x7f080011;
-        public static final int business_name_2=0x7f080014;
-        public static final int business_time_0=0x7f08000f;
-        public static final int business_time_1=0x7f080012;
-        public static final int business_time_2=0x7f080015;
+        public static final int RelativeLayout1=0x7f08000b;
+        public static final int View1=0x7f08000c;
+        public static final int action_settings=0x7f080019;
+        public static final int app_layout=0x7f08000d;
+        public static final int app_map=0x7f08000f;
+        public static final int app_reservation=0x7f08000e;
+        public static final int business_layout_0=0x7f080010;
+        public static final int business_layout_1=0x7f080013;
+        public static final int business_layout_2=0x7f080016;
+        public static final int business_name_0=0x7f080011;
+        public static final int business_name_1=0x7f080014;
+        public static final int business_name_2=0x7f080017;
+        public static final int business_time_0=0x7f080012;
+        public static final int business_time_1=0x7f080015;
+        public static final int business_time_2=0x7f080018;
         public static final int header=0x7f080001;
         public static final int loginButton=0x7f080007;
         public static final int passwordEditText=0x7f080006;
         public static final int passwordLayout=0x7f080004;
         public static final int passwordTextView=0x7f080005;
+        public static final int tagin_layout=0x7f080008;
+        public static final int title=0x7f08000a;
+        public static final int toggle=0x7f080009;
         public static final int usernameEditText=0x7f080003;
         public static final int usernameLayout=0x7f080000;
         public static final int usernameTextView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int my_q=0x7f030001;
+        public static final int handler=0x7f030001;
+        public static final int my_q=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -59,6 +66,18 @@ public final class R {
         public static final int MyQ=0x7f050002;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int general_exception=0x7f05000e;
+        public static final int handle_view_name=0x7f050003;
+        public static final int message_is_too_big_for_tag=0x7f050008;
+        public static final int nfcAvailableDisabled=0x7f050006;
+        public static final int nfcAvailableEnabled=0x7f050005;
+        public static final int noNfcMessage=0x7f050007;
+        public static final int success=0x7f050009;
+        public static final int tag_does_not_support_ndef=0x7f05000b;
+        public static final int tag_in=0x7f050004;
+        public static final int tag_is_read_only=0x7f05000c;
+        public static final int tag_refused_to_connect=0x7f05000a;
+        public static final int tag_refused_to_format=0x7f05000d;
     }
     public static final class style {
         /** 
